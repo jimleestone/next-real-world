be111d98e2f66a5d389d42d0b003bc9b09a0862b
Jim <jimleestone@gmail.com>
2022-07-10 01:42:00 +0800

    vscode failed to connect to WSL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['s2.loli.net'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Footer from '../components/common/Footer';
+import Header from '../components/common/Header';
+import { AuthProvider, CustomApolloProvider } from '../lib/hooks/use-auth';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CustomApolloProvider>
+      <AuthProvider>
+        <Header />
+        <Component {...pageProps} />;
+        <Footer />
+      </AuthProvider>
+    </CustomApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
